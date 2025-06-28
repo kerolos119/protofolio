8829d8d577c4047d02f1d0d1996e0e4517cc8d44
kerolos119 <kerolosbotros99@gmail.com>
2025-06-28 14:00:55 +0300

    protofolio

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-const base='/kerllos-portfolio/'
-    
-// https://vite.dev/config/
+import { fileURLToPath } from 'url'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-[react()],
-  tailwindcss(),
-  base,
+  plugins: [react()],
+
+  base: '/portfolio/',
   
-  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./Pages', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+  },
 })
-
-
-
